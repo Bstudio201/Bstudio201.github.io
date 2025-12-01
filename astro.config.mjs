5832f4d1921b3d6a16de9625d30ca0c932b2f27b
백종현 <jonghyeonbaek2@gmail.com>
2025-12-01 21:11:07 +0900

    Update config for user site (remove base path)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Bstudio201.github.io',
-	base: '/studio-astro-site',
 	integrations: [mdx(), sitemap()],
 });
